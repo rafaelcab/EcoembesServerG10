601de0b7d4faeb4dd36368c33242472333423ebb
Rafael Caballero <rafael.caballero@opendeusto.es>
2025-11-14 19:01:30 +0100

    Initial commit

---
+++ src/main/java/com/ecoembes/EcoembesServer/EcoembesServerG10Application.java
@@ -0,0 +1,13 @@
+package com.ecoembes.EcoembesServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoembesServerG10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoembesServerG10Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoembes/EcoembesServer/EcoembesServerG10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoembes.EcoembesServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoembesServerG10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
